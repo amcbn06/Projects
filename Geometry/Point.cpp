91349a14fd44f7a19de0144e32f415a938033e3e
amcbn06 <89391344+amcbn06@users.noreply.github.com>
2024-05-13 23:54:35 +0300

    Create Point.cpp
    Point class for geometry problems

---
+++ Geometry/Point.cpp
@@ -0,0 +1,27 @@
+struct point {
+    ll x = 0, y = 0;
+    int index = 0;
+    point() {
+
+    }
+    point(ll x, ll y) {
+        this->x = x;
+        this->y = y;
+    }
+    friend point operator-(const point& a, const point& b) {
+        return point(a.x - b.x, a.y - b.y);
+    }
+    friend bool operator<(const point& a, const point& b) {
+        return make_pair(a.x, a.y) < make_pair(b.x, b.y);
+    }
+    friend double distance(const point& a, const point& b) {
+        return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+    }
+    friend ll cross(const point& a, const point& b) {
+        return a.x * b.y - a.y * b.x;
+    }
+    friend int orientation(const point& a, const point& b, const point& c) {
+        ll o = cross(b - a, c - a);
+        return (o > 0) - (o < 0);
+    }
+};
