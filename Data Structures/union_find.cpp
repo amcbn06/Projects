222b5a153686c37b55ceb3e2e35f7a1329fd6565
amcbn06 <89391344+amcbn06@users.noreply.github.com>
2024-06-19 17:35:07 +0300

    Create union_find.cpp
    Union Find / Disjoint Set Union

---
+++ Data Structures/union_find.cpp
@@ -0,0 +1,37 @@
+struct union_find {
+    vector<int> father, rank;
+    int size, components = 0;
+    union_find(int size = 0) {
+        this->resize(size);
+    }
+    void resize(int size) {
+        this->size = size;
+        father.resize(size);
+        rank.resize(size);
+    }
+    void clear() {
+        this->size = 0;
+        father.clear();
+        rank.clear();
+    }
+    int find(int x) {
+        return father[x] == x ? x : (father[x] = find(father[x]));
+    }
+    int merge(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) {
+            return;
+        }
+        components--;
+        if (rank[x] < rank[y]) {
+            swap(x, y);
+        }
+        father[y] = x;
+        rank[x] += rank[y];
+        rank[y] = 0;
+    }
+    bool connected(int x, int y) {
+        return find(x) == find(y);
+    }
+};
